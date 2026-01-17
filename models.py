91ccb786827cf89f7274ff218636993280af8722
khushi <khushi@gmail.com>
2026-01-17 21:29:53 +0530

    minor change in nf

--- models.py
+++ models.py
@@ -5,5 +5,5 @@ class Item(Base):
     __tablename__ = "items"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(255), index=True)
-    description = Column(String(255), index=True)
+    description = Column(String(200), index=True)
     price = Column(Integer)
